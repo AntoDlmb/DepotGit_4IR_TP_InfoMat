143c5efd872348e30ad6f02a1e4d78610f8ea0a6
AntoDlmb <76205125+AntoDlmb@users.noreply.github.com>
2021-11-11 19:25:34 +0100

    Add files via upload

---
+++ ProjetVoilierF3/Objects/adxl345.d
@@ -0,0 +1,10 @@
+.\objects\adxl345.o: ..\Mes Drivers\ADXL345.c
+.\objects\adxl345.o: ..\Mes Drivers\Driver_SPI.h
+.\objects\adxl345.o: C:\Programdata\Keil\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h
+.\objects\adxl345.o: .\RTE\_Simul_\RTE_Components.h
+.\objects\adxl345.o: C:\Programdata\Keil\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm3.h
+.\objects\adxl345.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\adxl345.o: C:\Programdata\Keil\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\adxl345.o: C:\Programdata\Keil\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\adxl345.o: C:\Programdata\Keil\Arm\Packs\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\adxl345.o: C:\Programdata\Keil\Arm\Packs\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h
---
+++ ProjetVoilierF3/Source/Principal.c
@@ -0,0 +1,74 @@
+#include"stm32f10x.h"
+#include"MyTimer.h"
+#include"Driver_GPIO.h"
+#include"Driver_USART.h"
+#include "Driver_SPI.h"
+#define USART_TX 9
+#define USART_RX 10 //D2
+#define PWM_PIN 8 //D7
+#define SENS_PIN 9 //D8
+#define TIMER TIM1
+MyGPIO_Struct_TypeDef mGPIOsens = {
+		GPIOA,
+		SENS_PIN,
+		Out_Ppull
+	};
+
+void Callback(char data) {
+	char sens = (data & 0x80)>>7;
+	char alpha = data & 0x7F;
+	
+	if (sens){
+		alpha=128-alpha;
+		MyGPIO_Set (mGPIOsens.GPIO, SENS_PIN);
+	}else{
+		MyGPIO_Reset (mGPIOsens.GPIO, SENS_PIN);
+	}
+	MyPWM_SetRatio(TIMER,1,alpha);
+	
+} 
+
+
+int main(void){
+	
+	MyTimer_Struct_TypeDef MyTimer = {TIMER , 3599, 0};
+	
+
+	MyGPIO_Struct_TypeDef mGPIOUSART = {
+		GPIOA,
+		USART_RX,
+		In_Floating
+	};
+	
+	MyGPIO_Struct_TypeDef mGPIOPWM = {
+		GPIOA,
+		PWM_PIN,
+		AltOut_Ppull
+	};
+	
+	
+	
+	
+		
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN;
+	RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+	USART_Base_Init();
+
+	MyGPIO_Init(&mGPIOUSART);
+	MyGPIO_Init(&mGPIOPWM);
+	MyGPIO_Init(&mGPIOsens);
+	
+	//Comment gérer la clock ?
+	//faire interruption régulières  pour Read_Acceleration
+	SPI_Base_Init();
+
+	MyTimer_Base_Init(&MyTimer);
+	MyPWM_Enable(TIMER,1);
+	MY_TIMER_BASE_START(TIMER);
+	USART_ActiveReadIT(1, Callback);
+	do{
+			
+	}while(1);
+}
